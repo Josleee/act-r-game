04c746954ea019129fff9b1aaed59dbe1d5d78a8
Josleee <cz2f@qq.com>
2018-03-16 02:38:25 +0100

    Add model folder

---
+++ painting_cheat_game/game_demo/Models/BaseGame.swift
@@ -0,0 +1,17 @@
+//
+//  BaseGame.swift
+//  game_demo
+//
+//  Created by Jed on 16/03/2018.
+//  Copyright © 2018 Jed. All rights reserved.
+//
+
+import Foundation
+
+class BaseGame {
+    
+    init() {
+        
+    }
+    
+}
