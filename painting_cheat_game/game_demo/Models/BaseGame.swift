158a3275b82e71fb3fa57cf13ee959909a8c7020
Josleee <cz2f@qq.com>
2018-03-16 12:19:17 +0100

    Add game base function

--- painting_cheat_game/game_demo/Models/BaseGame.swift
+++ painting_cheat_game/game_demo/Models/BaseGame.swift
@@ -8,10 +8,66 @@
 
 import Foundation
 
+enum RunTimeError: Error {
+    case invalidAmount(Int)
+}
+
 class BaseGame {
     
+    private var humanCoinsAmount : Int!
+    private var AICoinsAmount : Int!
+    private var coinsAmountInPot : Int = 0
+    
+    private var humanPaintingValue : Int = 0
+    private var AIPaintingValue : Int = 0
+    
     init() {
+        humanCoinsAmount = 20
+        AICoinsAmount = 20
         
     }
     
+    func setPainting(humanPainting : Int, AIPainintg : Int) {
+        humanPaintingValue = humanPainting
+        AIPaintingValue = AIPainintg
+    }
+    
+    func raise(amountCoins : Int, isHumanPlayer : Bool) throws {
+        if (isHumanPlayer) {
+            if (humanCoinsAmount < amountCoins) {
+                throw RunTimeError.invalidAmount(amountCoins)
+            }
+            humanCoinsAmount! -= amountCoins
+            coinsAmountInPot += amountCoins
+        } else {
+            if (AICoinsAmount < amountCoins) {
+                throw RunTimeError.invalidAmount(amountCoins)
+            }
+            AICoinsAmount! -= amountCoins
+            coinsAmountInPot += amountCoins
+        }
+    }
+    
+    func fold(isHumanPlayer : Bool) {
+        if (isHumanPlayer) {
+            AICoinsAmount! += coinsAmountInPot
+        } else {
+            humanCoinsAmount! += coinsAmountInPot
+        }
+        coinsAmountInPot = 0
+    }
+    
+    func endGame() -> Int {
+        if (humanPaintingValue > AIPaintingValue) {
+            humanCoinsAmount! += coinsAmountInPot
+            coinsAmountInPot = 0
+            return 1
+        } else if (humanPaintingValue < AIPaintingValue) {
+            AICoinsAmount! += coinsAmountInPot
+            coinsAmountInPot = 0
+            return -1
+        } else {
+            return 0
+        }
+    }
 }
