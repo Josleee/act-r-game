e55bc3f4d23e5e970b62b91b20ff0ccbbcceb023
Josleee <cz2f@qq.com>
2018-03-17 15:59:44 +0100

    Optimize menu page

--- painting_cheat_game/game_demo/Scenes/GameMenu/GameScene.swift
+++ painting_cheat_game/game_demo/Scenes/GameMenu/GameScene.swift
@@ -10,23 +10,27 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    
+
     override func didMove(to view: SKView) {
-        let myLabel = SKLabelNode(fontNamed: "Chalkduster")
-        myLabel.text = "Welcome to Painting Cheat @~@"
-        myLabel.fontSize = 30
         // print(self.frame.size.width)
         // print(self.frame.size.height)
-        myLabel.position = CGPoint(x: 0, y: 0)
+        let myLabel = SKLabelNode(fontNamed: "Chalkduster")
+        myLabel.text = "Welcome to Painting Cheat @~@"
+        myLabel.fontSize = 25
+        myLabel.position = CGPoint(x: 0, y: 10)
         self.addChild(myLabel)
+        
+        let startLabel = SKLabelNode(fontNamed: "Chalkduster")
+        startLabel.text = "Tap screen to start game"
+        startLabel.fontSize = 17
+        startLabel.position = CGPoint(x: 0, y: -35)
+        self.addChild(startLabel)
     }
     
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         let gameSceneTemp = MainGameScene(fileNamed: "MainGameScene")
-        self.scene?.view?.presentScene(gameSceneTemp!, transition: SKTransition.crossFade(withDuration: 1))
+        self.scene?.view?.presentScene(gameSceneTemp!, transition: SKTransition.flipVertical(withDuration: 1))
     }
     
     
