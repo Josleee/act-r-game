d4c69dd032aa7c669ed290e4268f02def694b407
E. Gassner <s3542041@ai178141.ai.rug.nl>
2018-03-27 16:36:46 +0200

    added first simple "rules screen" demo

--- painting_cheat_game/game_demo/Scenes/RulesScene/RulesScene.swift
+++ painting_cheat_game/game_demo/Scenes/RulesScene/RulesScene.swift
@@ -1,56 +1,54 @@
 //
-//  GameScene.swift
+//  RulesScene.swift
 //  game_demo
 //
-//  Created by Jed on 15/03/2018.
+//  Created by E. Gassner on 3/27/18.
 //  Copyright © 2018 Jed. All rights reserved.
 //
 
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
-    private var rulesBtn : SKLabelNode!
+class RulesScene: SKScene {
     private let moveNodeUp = SKAction.moveBy(x: 0, y: 10, duration: 0.2)
     private let moveNodeDown = SKAction.moveBy(x: 0, y: -10, duration: 0.2)
+    
+    private var startGameButton : SKLabelNode!
     
     override func didMove(to view: SKView) {
         // print(self.frame.size.width)
         // print(self.frame.size.height)
         let myLabel = SKLabelNode(fontNamed: "Chalkduster")
-        myLabel.text = "Welcome to Painting Cheat @~@"
+        myLabel.text = "Welcome to the rules"
         myLabel.fontSize = 25
         myLabel.position = CGPoint(x: 0, y: 10)
         self.addChild(myLabel)
         
-        let startLabel = SKLabelNode(fontNamed: "Chalkduster")
-        startLabel.text = "Tap screen to start game"
-        startLabel.fontSize = 17
-        startLabel.position = CGPoint(x: 0, y: -35)
-        self.addChild(startLabel)
-        
-        rulesBtn = SKLabelNode(fontNamed: "text")
-        rulesBtn.text = "Rules"
-        rulesBtn.fontSize = 65
-        rulesBtn.position = CGPoint(x: 270, y: -145)
-        rulesBtn.name = "btn_rules"
-        rulesBtn.zPosition = 1
-        self.addChild(rulesBtn)
+       // startGameButton = SKSpriteNode(imageNamed: "startGame")
+        startGameButton = SKLabelNode(fontNamed: "text")
+        //startGameButton.size = CGSize(width: (startGameButton.size.width / 2), height: (startGameButton.size.height / 2))
+        startGameButton.fontSize = 65
+        startGameButton.position = CGPoint(x: 270, y: -145)
+        startGameButton.name = "btn_startGame"
+        startGameButton.text = "Start Game"
+        startGameButton.fontSize = 65
+        startGameButton.zPosition = 1
+        self.addChild(startGameButton)
     }
     
     
-    
+    //if button to start game is pressed then start game
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
         let gameSceneTemp = MainGameScene(fileNamed: "MainGameScene")
-        let rulesSceneTemp = RulesScene(fileNamed: "RulesScene")
         let touch = touches.first
         
         if let location = touch?.location(in: self) {
             let nodeArray = self.nodes(at: location)
             
             //selected rules
-            if (nodeArray.first?.name == "btn_rules") {
-                print("RULES selected")
+            if (nodeArray.first?.name == "btn_startGame") {
+                print("selected start game BUTTON")
                 // Button animation
                 nodeArray.first?.run(moveNodeUp)
                 let wait = SKAction.wait(forDuration:0.25)
@@ -58,19 +56,8 @@ class GameScene: SKScene {
                     nodeArray.first?.run(self.moveNodeDown)
                 }
                 run(SKAction.sequence([wait,action]))
-                self.scene?.view?.presentScene(rulesSceneTemp!, transition: SKTransition.flipVertical(withDuration: 1))
-                
-            } else { //maingame
                 self.scene?.view?.presentScene(gameSceneTemp!, transition: SKTransition.flipVertical(withDuration: 1))
             }
-        
-        
-            
         }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
     }
 }
